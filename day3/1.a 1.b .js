445b4a98e2c425dfa8209900d73a5fc548b713b3
Fullstack-Devops-Engineer <vinothraj777@gmail.com>
2023-06-29 23:47:01 +0530

    first commit

--- day3/1.a 1.b .js
+++ day3/1.a 1.b .js
@@ -8,15 +8,28 @@
 
  let obj2={age:5,name:"Person 1"};
 
-//  console.log(Object.keys(obj1))   =====> to view the keys of the object
+//  console.log(Object.keys(obj1));   =====> to view the keys of the object
 
-//  console.log(Object.keys(obj2))   =====> to view the keys of the object
+//  console.log(Object.keys(obj2));   =====> to view the keys of the object
 
-//  console.log(Object.values(obj1)) =====> to view the values of the object
+//  console.log(Object.values(obj1)); =====> to view the values of the object
 
-//  console.log(Object.values(obj1)) =====> to view the values of the object
+//  console.log(Object.values(obj1)); =====> to view the values of the object
 
- console.log(JSON.stringify(obj1))   // =====> to convert json object to json string
+//  console.log(JSON.stringify(obj1));   // =====> to convert json object to json string
 
- console.log(JSON.stringify(obj2))   // =====> to convert json object to json string 
+//  console.log(JSON.stringify(obj2));  // =====> to convert json object to json string 
 
+// console.log(Object.keys(obj1).length); // =====> to view object keys length
+
+// console.log(obj1.hasOwnProperty('name')); // =====> to check the specific key is present in object or not
+
+
+var result=Object.keys(obj1).length !==Object.keys(obj2)
+
+var result1=Object.keys(obj1).length ==Object.keys(obj2)
+// console.log(Object.keys(obj1).length )
+// console.log(Object.keys(obj2).length)
+console.log(result)
+console.log(result1)
+ 
